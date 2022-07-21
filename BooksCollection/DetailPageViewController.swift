2682954d9c524153619e33909c0bf9235bf5e4ba
skyler <ho971122@gmail.com>
2022-07-21 21:43:55 +0900

    도서 프로젝트 화면 전환 구현

--- BooksCollection/DetailPageViewController.swift
+++ BooksCollection/DetailPageViewController.swift
@@ -17,4 +17,13 @@ class DetailPageViewController: UIViewController {
         
     }
 
+    @IBAction func webButtonClicked(_ sender: UIButton) {
+        
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc = sb.instantiateViewController(withIdentifier: WebViewController.identifier) as! WebViewController
+        self.navigationController?.pushViewController(vc, animated: true)
+        
+        
+    }
+    
 }
---
+++ BooksCollection/WebViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WebViewController.swift
+//  BooksCollection
+//
+//  Created by 이현호 on 2022/07/21.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    static let identifier = "WebViewController"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
